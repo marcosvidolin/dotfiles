87213dbbfaefbab06fb1bbb84a1ae1b1f623eac5
Marcos Vidolin⚡ <marcosvidolin@gmail.com>
2024-05-25 13:05:55 +0100

    Update nvim-treesitter.lua

--- .config/nvim/lua/vidolin/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/vidolin/plugins/nvim-treesitter.lua
@@ -1,10 +1,53 @@
 return {
-	-- Highlight, edit, and navigate code
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	build = function()
-		pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-	end,
+  -- Highlight, edit, and navigate code
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  opts = {
+    auto_install = true,
+    autotag = {
+      enable = true,
+      filetypes = {
+        "html",
+        "javascript",
+        "typescript",
+        "svelte",
+        "vue",
+        "tsx",
+        "jsx",
+        "rescript",
+        "css",
+        "lua",
+        "xml",
+        "php",
+        "markdown",
+      },
+    },
+    indent = { enable = true },
+    ensure_installed = {
+      -- defaults
+      "vim",
+      "lua",
+      "vimdoc",
+
+      -- web dev
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "astro",
+      "vue",
+      "svelte",
+      "markdown",
+      "markdown_inline",
+      "json",
+      "scss",
+      "yaml",
+    },
+  },
+  build = function()
+    pcall(require("nvim-treesitter.install").update({ with_sync = true }))
+  end,
 }
